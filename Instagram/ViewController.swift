32ef0aec37a0c455adf2eca91d5d1df6c93372c2
yshiina1102 <yshiina@salesforce.com>
2020-05-06 18:11:03 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Yusuke Shiina on 2020/05/06.
+//  Copyright © 2020 Yusuke Shiina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
