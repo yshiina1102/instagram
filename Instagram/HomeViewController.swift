7a861727e942bfabcceba495003f3bf33892cd06
yshiina1102 <yshiina@salesforce.com>
2020-05-06 18:54:28 +0900

    commit 2nd 20200506

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Instagram
 //
 //  Created by Yusuke Shiina on 2020/05/06.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +17,3 @@ class ViewController: UIViewController {
 
 
 }
-
---
+++ Instagram/TabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by Yusuke Shiina on 2020/05/06.
+//  Copyright © 2020 Yusuke Shiina. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // タブアイコンの色
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        // タブバーの背景色
+        self.tabBar.barTintColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する。
+        self.delegate = self
+    }
+
+    // タブバーのアイコンがタップされた時に呼ばれるdelegateメソッドを処理する。
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController is ImageSelectViewController {
+            // ImageSelectViewControllerは、タブ切り替えではなくモーダル画面遷移する
+            let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelect")
+            present(imageSelectViewController, animated: true)
+            return false
+        } else {
+            // その他のViewControllerは通常のタブ切り替えを実施
+            return true
+        }
+    }
+
+}
